7212253f43602dc674b8989f0f9dd01b95ed46c5
Junnosuke302 <junnosuke68@gmail.com>
2021-12-09 14:09:18 +0900

    optional chaining

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import axios from 'axios';
 import { useState } from 'react';
 import './App.css';
+import { Text } from './Text';
 import { Todo } from './Todo';
 import { TodoType } from "./types/todo";
+import { UserProfile } from './UserProfile';
+
+const user = {
+    name: "asdf",
+    // hobbies: ["qwer", "zxcv"],
+};
 
 function App() {
     const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -19,6 +26,8 @@ function App() {
   
     return (
         <div className="App">
+            <UserProfile user={user} />
+            <Text color="red" fontSize="18px" />
             <button onClick={onClickFetchData}>データ取得</button>
             {todos.map((todo) => (
                 <Todo
