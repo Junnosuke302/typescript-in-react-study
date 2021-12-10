e8933002c32931df798e7bbb750a66fa6648e378
Junnosuke302 <junnosuke68@gmail.com>
2021-12-10 20:46:00 +0900

    login-function-added

---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,28 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useCallback, useState } from "react";
+
+import { user } from "../types/api/user";
+import { useMessage } from "./useMessage";
+
+export const useAllUsers = () => {
+    const { showMessage } = useMessage();
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [users, setUsers] = useState<Array<user>>([]);
+
+    const getUsers= useCallback(() => {
+        setLoading(true);
+        axios
+            .get<Array<user>>("https://jsonplaceholder.typicode.com/users")
+            .then((res) => setUsers(res.data))
+            .catch(() => {
+                showMessage({ title: "ユーザー取得に失敗しました", status: "error" });
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    return { getUsers, loading, users };
+};
